f4b4cae47907a1e6ef32b51345a5e0308b34a870
Trevor Kumec <tkumec@ucsc.edu>
2022-02-10 11:16:16 -0800

    reading PMW and YCWA data with readxl

--- working_script.R
+++ working_script.R
@@ -1,2 +1,31 @@
-git config --global user.name "Trevor Kumec"
-git config --global user.email tkumec@ucsc.edu
+library(tidyverse)
+library(readxl)
+
+##Read in PMW manual data, using readxl because there are many sheets
+read_excel("data/PMW_manual.xlsx")
+
+path <- "data/PMW_manual.xlsx"
+path
+
+pmw_data <- path %>% 
+  excel_sheets() %>%
+  set_names() %>%
+  map_dfr(read_excel, path=path)
+
+pmw_data <- test %>%
+  select(-("...1"))
+
+pmw_data <- pmw_data %>%
+  mutate(dataset = "PMW")
+
+##Read YMA data
+
+path2 <- "data/YCWA_manual.xlsx"
+
+ycwa_data <- path2 %>% 
+  excel_sheets() %>%
+  set_names() %>%
+  map_dfr(read_excel, path=path2)
+
+ycwa_data <- ycwa_data %>%
+  mutate(dataset = "YCWA")
