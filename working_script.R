3a1e01d6413c457b1c95e8bdc1ec9c2cabf66fff
Trevor Kumec <tkumec@ucsc.edu>
2022-02-15 11:17:57 -0800

    Add day, mo, year, columns

--- working_script.R
+++ working_script.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(readxl)
+library(lubridate)
 
 ##Read in PMW manual data, using readxl because there are many sheets
 read_excel("data/PMW_manual.xlsx")
@@ -12,8 +13,6 @@ pmw_data <- path %>%
   set_names() %>%
   map_dfr(read_excel, path=path)
 
-pmw_data <- test %>%
-  select(-("...1"))
 
 pmw_data <- pmw_data %>%
   mutate(dataset = "PMW")
@@ -30,7 +29,30 @@ ycwa_data <- path2 %>%
 ycwa_data <- ycwa_data %>%
   mutate(dataset = "YCWA")
 
-##Join the tables
+##Bind the tables
 
 combined_welldata <- rbind(pmw_data, ycwa_data)
+
+##1963 observation needs to go 
+exclude_1963 <- filter(combined_welldata, Date > 1980)
+
+exclude_1963 %>%
+  ggplot(aes(x=Date, y=GW_Below_RP, color = SWN)) +
+  geom_point() +
+  facet_wrap("dataset")
+
+#####USGS streamflow data
+
+raw_gauge_data <- read.table("data/yuba_cfs_2016_2021.txt", sep="\t", header=TRUE)
+
+raw_gauge_data <- select(raw_gauge_data, -c("X15678_00060_cd"))
+raw_gauge_data <- raw_gauge_data[-c(1),]
+
+##Fix datetime - Adding columns for each component
+
+raw_gauge_data <- raw_gauge_data %>%
+  mutate(Year = year(datetime)) %>%
+  mutate(Month = month(datetime)) %>%
+  mutate(Day = day(datetime)) %>%
+  mutate(Time = minute(datetime))
 
