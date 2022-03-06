7eb359328faa8287e73bd85763281ae3b373c8b2
Trevor Kumec <tkumec@ucsc.edu>
2022-03-05 16:51:22 -0800

    Added wateryear

--- working_script.R
+++ working_script.R
@@ -34,12 +34,12 @@ ycwa_data <- ycwa_data %>%
 combined_welldata <- rbind(pmw_data, ycwa_data)
 
 ##1963 observation needs to go 
-exclude_1963 <- filter(combined_welldata, Date > 1980)
+combined_welldata <- filter(combined_welldata, Date > 1980)
 
-exclude_1963 %>%
+combined_welldata %>%
   ggplot(aes(x=Date, y=GW_Below_RP, color = SWN)) +
   geom_point() +
-  facet_wrap("dataset")
+  facet_wrap("SWN")
 
 #####USGS streamflow data
 
@@ -48,11 +48,44 @@ raw_gauge_data <- read.table("data/yuba_cfs_2016_2021.txt", sep="\t", header=TRU
 raw_gauge_data <- select(raw_gauge_data, -c("X15678_00060_cd"))
 raw_gauge_data <- raw_gauge_data[-c(1),]
 
-##Fix datetime - Adding columns for each component
+##Fix datetime for USGS gauge data- Adding columns for each component
 
 raw_gauge_data <- raw_gauge_data %>%
   mutate(Year = year(datetime)) %>%
   mutate(Month = month(datetime)) %>%
   mutate(Day = day(datetime)) %>%
   mutate(Time = minute(datetime))
+
+combined_welldata <- combined_welldata %>%
+  mutate(Year = year(Date)) %>%
+  mutate(Month = month(Date)) %>%
+  mutate(Day = day(Date)) %>%
+  mutate(Time = minute(Date))
+
+
+##Create water year for well data
+
+wtr_yr <- function(dates, start_month=9) {
+  # Convert dates into POSIXlt
+  dates.posix = as.POSIXlt(dates)
+  # Year offset
+  offset = ifelse(dates.posix$mon >= start_month - 1, 1, 0)
+  # Water year
+  adj.year = dates.posix$year + 1900 + offset
+  # Return the water year
+  adj.year
+}
+
+
+water_year <- wtr_yr(combined_welldata$Date, 10)
+df = data.frame(combined_welldata, wtr_yr=wtr_yr(combined_welldata$Date, 10))
+colnames(df) <- c("Date", "water_year")
+
+ 
+
+
+##Experiment with grouping
+
+
+
 
